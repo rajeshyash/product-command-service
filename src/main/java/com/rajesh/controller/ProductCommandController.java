b9ed1b724b451a549e54db7d4bef99dfa67037f3
Rajesh Sharma <Rajesh@Apple.local>
2023-12-29 12:20:24 +0530

    product-command-service

---
+++ src/main/java/com/rajesh/controller/ProductCommandController.java
@@ -0,0 +1,25 @@
+package com.rajesh.controller;
+
+import com.rajesh.dto.ProductEvent;
+import com.rajesh.entity.Product;
+import com.rajesh.service.ProductCommandService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/products")
+public class ProductCommandController {
+
+    @Autowired
+    private ProductCommandService commandService;
+
+    @PostMapping
+    public Product createProduct(@RequestBody ProductEvent productEvent) {
+        return commandService.createProduct(productEvent);
+    }
+
+    @PutMapping("/{id}")
+    public Product updateProduct(@PathVariable long id, @RequestBody ProductEvent productEvent) {
+        return commandService.updateProduct(id, productEvent);
+    }
+}
---
+++ src/main/java/com/rajesh/service/ProductCommandService.java
@@ -0,0 +1,37 @@
+package com.rajesh.service;
+
+import com.rajesh.dto.ProductEvent;
+import com.rajesh.entity.Product;
+import com.rajesh.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductCommandService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    @Autowired
+    private KafkaTemplate<String, Object> kafkaTemplate;
+
+    public Product createProduct(ProductEvent productEvent) {
+        Product productDO = repository.save(productEvent.getProduct());
+        ProductEvent event = new ProductEvent("CreateProduct", productDO);
+        kafkaTemplate.send("product-event-topic", event);
+        return productDO;
+    }
+
+    public Product updateProduct(long id, ProductEvent productEvent) {
+        Product existingProduct = repository.findById(id).get();
+        Product newProduct = productEvent.getProduct();
+        existingProduct.setName(newProduct.getName());
+        existingProduct.setPrice(newProduct.getPrice());
+        existingProduct.setDescription(newProduct.getDescription());
+        Product productDO = repository.save(existingProduct);
+        ProductEvent event = new ProductEvent("UpdateProduct", productDO);
+        kafkaTemplate.send("product-event-topic", event);
+        return productDO;
+    }
+}
